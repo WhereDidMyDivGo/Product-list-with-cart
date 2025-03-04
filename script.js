9b6cb3632f1771f16b6e9626e19a76667c35d747
WhereDidMyDivGo <ahmadgameover3@gmail.com>
2025-03-04 15:01:24 +0400

    added products, working on functionality

--- script.js
+++ script.js
@@ -100,4 +100,68 @@ const products = [
   },
 ];
 
-console.log(products);
+const order = [];
+
+const productsElement = document.querySelector(".products");
+
+let result = [];
+
+products.forEach((product) => {
+  const { image, category, name, price } = product;
+  result += `
+    <div class="product">
+      <img
+        src="${image.mobile}"
+        alt="${name}"
+        class="product-img"
+      />
+      <button class="add-btn" value="${name}">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="21"
+          height="20"
+          fill="none"
+          viewBox="0 0 21 20"
+        >
+          <g fill="#C73B0F" clip-path="url(#a)">
+            <path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z" />
+            <path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z" />
+          </g>
+          <defs>
+            <clipPath id="a">
+              <path fill="#fff" d="M.333 0h20v20h-20z" />
+            </clipPath>
+          </defs>
+        </svg>
+        Add to Cart
+      </button>
+      <div class="text">
+        <h3 class="category">${category}</h3>
+        <div class="name">${name}</div>
+        <div class="price">$${price.toFixed(2)}</div>
+      </div>
+    </div>
+  `;
+});
+
+productsElement.innerHTML = result;
+
+const addBtns = document.querySelectorAll(".add-btn");
+const addItemHandler = (productName) => {
+  const clickedProduct = products.find((item) => productName === item.name);
+
+  const isInOrder = order.find((item) => item.name === clickedProduct.name);
+
+  if (isInOrder) {
+    isInOrder.amount++;
+  } else {
+    order.push({ ...clickedProduct, amount: 1 });
+  }
+  console.log(order);
+};
+
+addBtns.forEach((buttonElement) => {
+  buttonElement.addEventListener("click", () => {
+    addItemHandler(buttonElement.value);
+  });
+});
